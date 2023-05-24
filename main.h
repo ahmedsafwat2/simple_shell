091beae30540252aa1e52f84fc9e14694afbd8b9
ahmed <asosnses@gmail.com>
2023-05-24 16:05:34 -0400

    test 2 after some changes in betty

--- main.h
+++ main.h
@@ -23,7 +23,7 @@ extern char **environ;
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-                0, 0, 0}
+			0, 0, 0}
 /**
  * struct liststr - known as list_node
  * @num: the number field
